80eb1c814f929ba6e5ec027bab460135f8e19d9a
Ramiro Pinchao <antonio.pinchao@utp.edu.co>
2025-02-27 17:37:00 -0500

    I made some corrections to the code, correctly providing the total cost between two nodes

--- Backtracking y SDL version 1.pl
+++ Backtracking y SDL version 1.pl
@@ -1,44 +1,33 @@
 conexion(vancouver, edmonton, 16).
 conexion(vancouver, calgary, 13).
-
 conexion(edmonton, saskatoon, 12).
-
 conexion(calgary, edmonton, 4).
 conexion(calgary, regina, 14).
-
 conexion(saskatoon, winnipeg, 20).
 conexion(saskatoon, calgary, 9).
-
 conexion(regina, saskatoon, 7).
 conexion(regina, winnipeg, 4).
 
-
 tieneAristas(X) :- conexion(X, _, _).
-costoPasandoPorEntre(X, Y, Z):- 
-    conexion(X, Y, A ), 
-    conexion( Y, Z, B ),
-    C is A + B,
-    write(C),nl.
-	
 
-conexionEntre(X, Y, _ ) :- conexion( X, Y, _ ).
+conexionEntre(X, Y, _) :- conexion(X, Y, _).
 
 conexionEntre(X, Z, Visitados) :- 
-    conexion( X, Y, _ ), 
-    \+ member(Y, Visitados),
-    conexionEntre( Y, Z, [X|Visitados] ).
+    conexion(X, Y, _), 
+    \+ member(Y, Visitados),   
+    conexionEntre(Y, Z, [X|Visitados]).
 
-costoEntre(X, Y, Costo) :- 
-    costoEntre(X, Y, [X], 0, Costo).
+costoEntre(X, Y) :- 
+    costoEntreAux(X, Y, [X],  0, Costo), 
+    write('Costo total: '), write(Costo), nl.
 
-costoEntre(X, Y, _, Acumulado, Costo) :- 
+
+costoEntreAux(X, Y, _ , CostoAcumulado, CostoFinal) :- 
     conexion(X, Y, C),
-    Costo is Acumulado + C,
-    write(X), write(' -> '), write(Y), write(' | Costo total: '), write(Costo), nl.
+    CostoFinal is CostoAcumulado + C.
 
-costoEntre(X, Y, Visitados, Acumulado, Costo) :- 
+costoEntreAux(X, Y, Visitados, CostoAcumulado, CostoFinal) :- 
     conexion(X, Z, C),
-    \+ member(Z, Visitados),
-    NuevoCosto is Acumulado + C,
-    write(X), write(' -> '), write(Z), write(' | Costo acumulado: '), write(NuevoCosto), nl,
-    costoEntre(Z, Y, [Z|Visitados], NuevoCosto, Costo).
+    \+ member(Z, [X|Visitados]),  
+    NuevoCosto is CostoAcumulado + C,
+    costoEntreAux(Z, Y, [Z | Visitados ] , NuevoCosto, CostoFinal).
